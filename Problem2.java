b812220b8712bc964d5166812cc4a2a6ec976205
SeungYeapLee <38101772+SeungYeapLee@users.noreply.github.com>
2018-04-18 15:53:12 +0000

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -2,9 +2,9 @@
 public class Problem2 {
 	
 static int pow(int n, int m){int i, o=n;
-							 for(i=1;i<m;i++) n=n*o; return n;}
+	                     for(i=1;i<m;i++) n=n*o; return n;}
 
 public static void main(String[] args){int n=2, m=10; 
-System.out.println(pow(n, m));}
+                                       System.out.println(pow(n, m));}
 
 }
