da7b1256e690962d519ecf774dce097808d4d66e
SeungYeapLee <38101772+SeungYeapLee@users.noreply.github.com>
2018-04-18 15:52:33 +0000

    Add files via upload

---
+++ Problem2.java
@@ -0,0 +1,10 @@
+
+public class Problem2 {
+	
+static int pow(int n, int m){int i, o=n;
+							 for(i=1;i<m;i++) n=n*o; return n;}
+
+public static void main(String[] args){int n=2, m=10; 
+System.out.println(pow(n, m));}
+
+}
