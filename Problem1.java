6cc17bd13ab416e42f1cc19d6e7eed547f7b598f
SeungYeapLee <38101772+SeungYeapLee@users.noreply.github.com>
2018-04-18 15:59:51 +0000

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,9 +1,9 @@
 
 public class Problem1 {
 		
-static void privist(String n, int m){int i; 
-				    for(i=1;i<=m;i++) System.out.println(n);}
-
 public static void main(String[] args){ privist("a",3); }
+
+static void privist(String n, int m){int i; 
+				     for(i=1;i<=m;i++) System.out.println(n);}
 		
 	}
